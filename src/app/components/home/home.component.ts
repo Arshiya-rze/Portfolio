da904e47f0c06d4809f0bcfa0c38d738e7ea3c84
Arshiya Rezaie <103273356+Arshiya-rze@users.noreply.github.com>
2025-08-20 19:51:08 +0330

    design banner done without video

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from "../banner/banner.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    BannerComponent, BannerComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
