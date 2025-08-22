fe58e737b72d49c2272e4facc7ebeec370f4f49e
Arshiya Rezaie <103273356+Arshiya-rze@users.noreply.github.com>
2025-08-22 14:38:55 +0330

    create experience component and added to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { BannerComponent } from "../banner/banner.component";
 import { HeaderComponent } from "../header/header.component";
 import { AboutComponent } from "../about/about.component";
+import { ExperienceComponent } from '../experience/experience.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    BannerComponent, BannerComponent,
-    HeaderComponent, AboutComponent,
-    AboutComponent
+    BannerComponent, BannerComponent, HeaderComponent,
+    AboutComponent, AboutComponent, ExperienceComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
